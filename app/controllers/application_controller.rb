4d15ad9865dfb2839baa3e4d68931fd23081b702
Howard Kwon <howie.ykwon70@gmail.com>
2021-09-13 01:45:12 -0400

    commit tree 1c319f0f22eb414668340aa5f79a9e9551eb925a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,24 @@
 class ApplicationController < Sinatra::Base
 
+  set :default_content_type, 'application/json'
+
   get '/' do
     { message: "Hello world" }.to_json
+  end
+
+  get '/games' do
+    # get all the games from the database
+    # return a JSON response with an array of all the game data
+    games = Game.all.order(:title).limit(10)
+    games.to_json
+  end
+
+  # use the :id syntax to create a dynamic route
+  get '/games/:id' do
+    # look up the game in the database using its ID
+    # send a JSON-formatted response of the game data
+    game = Game.find(params[:id])
+    game.to_json(include: { reviews: { include: :user } })
   end
 
 end
